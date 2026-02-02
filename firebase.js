961c1cc29d4ca9ab1873f2efe7a16aa69719df54
omarabuzor71-dev <omarabuzor71@gmail.com>
2026-02-02 12:26:11 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+<script>const firebaseConfig = {
+  apiKey: "AIzaSyAxPGsIXuT72IJvvYrDJtoK3A2enNlI3Ak",
+  authDomain: "cashsyri-wallet.firebaseapp.com",
+  projectId: "cashsyri-wallet",
+  storageBucket: "cashsyri-wallet.firebasestorage.app",
+  messagingSenderId: "682839116596",
+  appId: "1:682839116596:web:710e98cbbb2b6270f97361",
+  measurementId: "G-WY6CV5PCKY"
+};
